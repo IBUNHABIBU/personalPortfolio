0a8ac2e327bfb7c231c137b7e7719daf7a3c564e
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-15 14:53:36 +0300

    styling the project section

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const navBtns = document.querySelectorAll('.nav-btns')
-navBtns.forEach( el => el.addEventListener('click',{
-  
-}))
+// const makeChange = (e) => {
+//   console.log(e.target)
+// }
+// Array.from(navBtns).forEach( el => console.log(el));
+console.log(navBtns)
+const header = document.querySelector('header');
+const changeColor = (e) => {
+  if(e.target.tagName === 'A') {
+    e.target.style.color = 'blue';
+  }
+}
+header.addEventListener('click', changeColor)
