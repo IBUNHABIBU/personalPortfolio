4823fc5cfad2dcf799268415bdb41e10f80b9d92
IBUNHABIBU <ibunhabib@gmail.com>
2022-02-24 12:12:01 +0300

    Adding file loader

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+const imtag = document.createElement('div');
+imtag.innerHTML = '<img class="img-fluid" src="images/recipes.PNG" width="100%" height="100%"  alt="gridhome">'
+           
+ document.body.appendChild(imtag);
